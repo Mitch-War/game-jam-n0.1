73870bcd4d83cba851a783898a495499a933d9ce
Mitch-War <48067761+Mitch-War@users.noreply.github.com>
2020-05-09 21:00:25 +0200

    First Commit
    -Basic movement added!
    -PP added!
    -Rudementory models added!

---
+++ CRT EFFECT TEST/Assets/Custom/Scripts/PlayerMovement.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Horizontal Movement")]
+    public float moveSpeed = 10f;
+    public Vector3 direction;
+
+    [Header("Vertical Movement")]
+    public float jumpSpeed = 15f;
+    public float jumpDelay = 0.25f;
+    private float jumpTimer;
+
+    [Header("Components")]
+    public Rigidbody rb;
+    public LayerMask groundLayer;
+
+    [Header("Physics")]
+    public float maxSpeed = 7f;
+    public float linearDrag = 4f;
+    public float fallMultiplier = 5;
+
+
+    [Header("Collision")]
+    public bool onGround = false;
+    public float groundLength = 0.4f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        //Shoots a raycast to see if character is touching ground.
+        onGround = Physics.Raycast(transform.position, Vector3.down, groundLength, groundLayer);
+
+        if (Input.GetButtonDown("Jump")) 
+        {
+            jumpTimer = Time.time + jumpDelay;
+        }
+
+        direction = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+    }
+
+    //Executes all physics based stuff
+    void FixedUpdate()
+    {
+        MoveCharacter(direction.x);
+
+        if (Mathf.Abs(rb.velocity.x) > maxSpeed) 
+        {
+            rb.velocity = new Vector3(Mathf.Sign(rb.velocity.x) * maxSpeed, rb.velocity.y);
+        }
+
+        if (jumpTimer > Time.time && onGround) 
+        {
+            Jump();
+        }
+
+        ModifyPhysics();
+    }
+
+    //Moves the character
+    void MoveCharacter(float horizontal) 
+    {
+        rb.AddForce(Vector3.left * horizontal * moveSpeed);
+    }
+
+    //Makes the character feel less slippy by increasing drag when needed.
+    void ModifyPhysics() 
+    {
+        bool changingDirections = (direction.x > 0 && rb.velocity.x < 0) || (direction.x < 0 && rb.velocity.x > 0);
+
+        if (onGround)
+        {
+            if (Mathf.Abs(direction.x) < 0.4f || changingDirections)
+            {
+                rb.drag = linearDrag;
+            }
+            else
+            {
+                rb.drag = 0f;
+            }
+
+            rb.useGravity = false;
+        }
+        else 
+        {
+            rb.useGravity = true;
+            rb.drag = linearDrag * 0.15f;
+
+            if (rb.velocity.y < 0)
+            {
+                rb.AddForce(Physics.gravity * fallMultiplier );
+            }
+            else if (rb.velocity.y > 0 && !Input.GetButton("Jump")) 
+            {
+                rb.AddForce(Physics.gravity * (fallMultiplier / 2));
+            }
+        }
+
+
+    }
+
+    void Jump() 
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0, 0);
+        rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+
+        jumpTimer = 0;
+    }
+
+    //Just for debugging
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * groundLength);
+    }
+}
